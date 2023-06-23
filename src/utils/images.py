39e663d7ada1f94f8a91df7df5b31ea9646f6f5d
aahnik <daw@aahnik.dev>
2023-06-24 02:46:12 +0530

    easy timestamp extraction

--- src/utils/images.py
+++ src/utils/images.py
@@ -8,6 +8,10 @@ def upload_image_to(instance, filename):
     # Generate a timestamp
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     # Create the filename with the timestamp
-    new_filename = f"{name}_{timestamp}{ext}"
+    new_filename = f"{name}_{timestamp}_{ext}"
     # Return the path relative to MEDIA_ROOT
     return os.path.join("images", new_filename)
+
+
+def extract_timestamp(url: str):
+    return url.split("_")[-2]
