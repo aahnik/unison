7c667eca74dc56858d39501cbb0e1d50bd1c4e80
aahnik <daw@aahnik.dev>
2023-06-24 02:51:25 +0530

    auto set url for images click as image raw url

--- src/home/models.py
+++ src/home/models.py
@@ -1,23 +1,34 @@
 from django.core.exceptions import ValidationError
 from django.db import models
 from solo.models import SingletonModel
-from utils.images import upload_image_to
+from utils.images import upload_image_to, extract_timestamp
 
 # Create your models here.
 
 
 class ModelImage(models.Model):
     alt_text = models.CharField(max_length=512)
-    redirect_url = models.URLField()
+    redirect_url = models.CharField(max_length=512, blank=True, null=True, default="")
     image = models.ImageField(upload_to=upload_image_to)
 
     def __str__(self):
         return self.alt_text
 
+    def save(self, *args, **kwargs):
+        super().save(args, kwargs)
+        # print(self.redirect_url)
+        if self.redirect_url is None or extract_timestamp(
+            self.redirect_url
+        ) != extract_timestamp(self.image.url):
+            self.redirect_url = self.image.url
+            print(self.redirect_url)
+
+        super().save(args, kwargs)
+
 
 class Link(models.Model):
     title = models.CharField(max_length=256)
-    link = models.CharField(max_length=1024, default="/")
+    link = models.CharField(max_length=1024, default="")
 
     def __str__(self) -> str:
         return self.title
@@ -55,15 +66,14 @@ class FooterLink(Link):
 class CallToAction(models.Model):
     # cta sections in home page
     home_content = models.ForeignKey("HomeContent", on_delete=models.CASCADE)
-    title = models.CharField(max_length=256, default = "Call to Action Card")
+    title = models.CharField(max_length=256, default="Call to Action Card")
     description = models.CharField(max_length=1024)
     button_text = models.CharField(max_length=32)
-    
+    fa_icon_html = models.CharField(max_length=4096)
 
 
 class HomeContent(SingletonModel):
     map_embed_url = models.CharField(max_length=5000, default="helllo")
-
 
 
 class HomeConfig(SingletonModel):
