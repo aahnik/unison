46eae3555e5343dfd6c4e6c4fd90438374fbdad6
aahnik <daw@aahnik.dev>
2023-07-11 01:46:50 +0530

    multiple updates in models.py of home
    - make image and link as abstract models
    - home content model with defaults
    - home config renamed to site config
    - setting defaults wherevel possible, and allowing null, blank
    - add social link model, with auto detect of brand on save,
    automatic icons

--- src/home/models.py
+++ src/home/models.py
@@ -2,14 +2,19 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from solo.models import SingletonModel
 from utils.images import upload_image_to, extract_timestamp
+from urllib.parse import urlparse
+from consts.validators import FA_ICON_BRANDS_ALLOWED
 
 # Create your models here.
 
 
 class ModelImage(models.Model):
-    alt_text = models.CharField(max_length=512)
+    alt_text = models.CharField(max_length=512, default="image")
     redirect_url = models.CharField(max_length=512, blank=True, null=True, default="")
     image = models.ImageField(upload_to=upload_image_to)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.alt_text
@@ -32,6 +37,8 @@ class Link(models.Model):
     title = models.CharField(max_length=256)
     link = models.CharField(max_length=1024, default="")
 
+    class Meta:
+        abstract = True
     def __str__(self) -> str:
         return self.title
 
@@ -45,7 +52,7 @@ class GalleryImage(ModelImage):
 
 
 class NavLink(Link):
-    home_config = models.ForeignKey("HomeConfig", on_delete=models.CASCADE)
+    site_config = models.ForeignKey("SiteConfig", on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         if NavLink.objects.count() >= 7:
@@ -53,8 +60,23 @@ class NavLink(Link):
         super().save(*args, **kwargs)
 
 
+class SocialLink(Link):
+    site_config = models.ForeignKey("SiteConfig", on_delete=models.CASCADE)
+    icon_brand = models.CharField(max_length=50, blank=True)
+
+    def save(self, *args, **kwargs):
+        try:
+            icon_brand = urlparse(self.link).netloc.split(".")[-2]
+            assert icon_brand in FA_ICON_BRANDS_ALLOWED, "Unknown brand"
+            self.icon_brand = f"fa-brands fa-{icon_brand}"
+
+        except Exception as _:
+            self.icon_brand = "fa-solid fa-link"
+        super().save(args, kwargs)
+
+
 class FooterLinkCateg(models.Model):
-    home_config = models.ForeignKey("HomeConfig", on_delete=models.CASCADE)
+    site_config = models.ForeignKey("SiteConfig", on_delete=models.CASCADE)
     categ_title = models.CharField(max_length=100)
 
     def __str__(self):
@@ -71,20 +93,43 @@ class CallToAction(models.Model):
     title = models.CharField(max_length=256, default="Call to Action Card")
     description = models.CharField(max_length=1024)
     button_text = models.CharField(max_length=32)
-    fa_icon_html = models.CharField(max_length=4096)
+    fa_icon = models.CharField(
+        max_length=4096, default="fa-solid fa-wand-magic-sparkles"
+    )
+
+
+NEWSLETTER_ICON_CHOICES = [
+    ("fa-brands fa-whatsapp", "Whatsapp"),
+    ("fa-regular fa-envelope", "Email"),
+]
 
 
 class HomeContent(SingletonModel):
-    map_embed_url = models.CharField(max_length=5000, default="helllo")
+    map_embed_url = models.CharField(max_length=5000)
+    newsletter_signup_header = models.CharField(
+        max_length=256, default="Sign up for newsletter"
+    )
+    newsletter_signup_subtitle = models.TextField(
+        max_length=2048,
+        default="Stay up to date with the roadmap progress, announcements and exclusive discounts feel free to sign up with your email.",
+    )
+    newsletter_signup_placeholder = models.CharField(
+        max_length=256, default="Enter your email"
+    )
+    newsletter_icon = models.CharField(
+        max_length=100,
+        choices=NEWSLETTER_ICON_CHOICES,
+        default=NEWSLETTER_ICON_CHOICES[1][0],
+    )
 
 
-class HomeConfig(SingletonModel):
+class SiteConfig(SingletonModel):
     site_name = models.CharField(max_length=255, default="Temple Web")
     maintenance_mode = models.BooleanField(default=False)
     # seperate out map embed url from home config
     # home config is shared across all templates via context processors
-    favicon = models.ImageField(upload_to=upload_image_to)
-    logo = models.ImageField(upload_to=upload_image_to)
+    favicon = models.ImageField(upload_to=upload_image_to, null=True, blank=True)
+    logo = models.ImageField(upload_to=upload_image_to, null=True, blank=True)
 
     def __str__(self):
         return self.site_name + " Config"
