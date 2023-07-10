2f416c311caf2c5a4654842b8d957537595b3e60
aahnik <daw@aahnik.dev>
2023-07-11 01:47:26 +0530

    update home's admin page

--- src/home/admin.py
+++ src/home/admin.py
@@ -5,10 +5,13 @@ from solo.admin import SingletonModelAdmin
 from .models import (
     FooterLink,
     FooterLinkCateg,
-    HomeConfig,
+    SiteConfig,
     NavLink,
     CarouselImage,
     GalleryImage,
+    CallToAction,
+    HomeContent,
+    SocialLink,
 )
 
 
@@ -18,17 +21,41 @@ class NavLinkForm(forms.ModelForm):
         fields = ["title", "link"]
 
 
-class NavLinkInline(admin.StackedInline):
-    model = NavLink
-    form = NavLinkForm
+class CallToActionForm(forms.ModelForm):
+    class Meta:
+        model = CallToAction
+        fields = ["title", "description", "button_text", "fa_icon"]
+
+
+class CallToActionInline(admin.StackedInline):
+    model = CallToAction
+    form = CallToActionForm
     extra = 1
     min_num = 1
     validate_min = True
 
 
-@admin.register(HomeConfig)
-class HomeConfigAdmin(SingletonModelAdmin):
-    inlines = [NavLinkInline]
+@admin.register(HomeContent)
+class HomeContentAdmin(SingletonModelAdmin):
+    inlines = [CallToActionInline]
+
+
+class NavLinkInline(admin.StackedInline):
+    model = NavLink
+    form = NavLinkForm
+    extra = 0
+
+
+
+class SocialLinkInline(admin.StackedInline):
+    model = SocialLink
+    extra = 0
+
+
+
+@admin.register(SiteConfig)
+class SiteConfigAdmin(SingletonModelAdmin):
+    inlines = [NavLinkInline, SocialLinkInline]
 
 
 class FooterLinkInline(admin.StackedInline):
@@ -41,9 +68,6 @@ class FooterLinkCategAdmin(admin.ModelAdmin):
     inlines = [FooterLinkInline]
 
 
-# @admin.register(FooterLink)
-# class FooterLinkAdmin(admin.ModelAdmin):
-#     pass
 @admin.register(CarouselImage)
 class CarouselImageAdmin(admin.ModelAdmin):
     list_display = ["id", "alt_text", "image", "redirect_url"]
