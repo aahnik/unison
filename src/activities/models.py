9b8481a292ad019c7b7b1945eb40f784c4ed6553
aahnik <daw@aahnik.dev>
2023-07-11 01:40:57 +0530

    create activity model with sane defaults

--- src/activities/models.py
+++ src/activities/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from utils.images import upload_image_to
+
 # Create your models here.
 
-class CommunityActivity(models.Model):
-    title = models.CharField(max_length=512)
-    summary = models.CharField(max_length=1024)
-    # icon may be
-    description = models.TextField(max_length=10240)
-    cover_image = models.ImageField(upload_to=upload_image_to)
 
-    
+class CommunityActivity(models.Model):
+    title = models.CharField(max_length=512, default="Group Study")
+    summary = models.CharField(max_length=1024, default="Activity description in short")
+
+    description = models.TextField(
+        max_length=10240, default="Detailed activity description"
+    )
+    fa_icon = models.TextField(max_length=100, default="fa-solid fa-graduation-cap")
+    cover_image = models.ImageField(upload_to=upload_image_to, null=True, blank=True)
