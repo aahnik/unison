969519392c2357aeae4c10c99800c0da0751ff95
aahnik <daw@aahnik.dev>
2023-07-11 01:40:32 +0530

    slience collectstatic

--- scripts/live.sh
+++ scripts/live.sh
@@ -12,7 +12,7 @@ cd src
 # do the migrations
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic -c --no-input
 
 # run the server
 python manage.py runserver
