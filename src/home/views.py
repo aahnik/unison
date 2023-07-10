e8a3255994834136b037fc204301fbcc2ac8b675
aahnik <daw@aahnik.dev>
2023-07-10 19:40:59 +0530

    fix code duplication in views

--- src/home/views.py
+++ src/home/views.py
@@ -1,14 +1,16 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
-from .models import CarouselImage, GalleryImage
+from .models import CarouselImage, GalleryImage, HomeContent
 
 
 def index(request):
-    carousel_images = CarouselImage.objects.all()
-    gallery_images = GalleryImage.objects.all()
+    context = {
+        "carousel_images": CarouselImage.objects.all(),
+        "gallery_images": GalleryImage.objects.all(),
+        "home_content": HomeContent.get_solo(),
+    }
 
-    context = {"carousel_images": carousel_images, "gallery_images": gallery_images}
     return render(request, "home/index.html", context=context)
 
 
