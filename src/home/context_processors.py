bd2ae1853b99e3ecfd02cc139bed00d5d0fe9ea8
aahnik <daw@aahnik.dev>
2023-07-11 01:42:01 +0530

    add social links to context_processors.py

--- src/home/context_processors.py
+++ src/home/context_processors.py
@@ -1,11 +1,12 @@
-from .models import HomeConfig, NavLink, FooterLinkCateg
+from .models import SiteConfig, NavLink, FooterLinkCateg, SocialLink
 
 
 def site_ctx(request):
     context = {
-        "home_config": HomeConfig.get_solo(),
+        "site_config": SiteConfig.get_solo(),
         "nav_links": NavLink.objects.all(),
         "footer_categories": FooterLinkCateg.objects.all(),
+        "social_links": SocialLink.objects.all()
     }
 
     return context
