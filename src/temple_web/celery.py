f851a56def3a2c5dfcab6b99feea941e2c50cd1b
aahnik <daw@aahnik.dev>
2024-01-02 03:13:16 +0530

    fix module name in celery.py
    this broke production run

--- src/temple_web/celery.py
+++ src/temple_web/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_celery.settings")
-app = Celery("django_celery")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "temple_web.settings")
+app = Celery("temple_web")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
