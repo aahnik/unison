43308bf3d9d0ca89321e789d9d45b27017ed8250
aahnik <daw@aahnik.dev>
2024-12-30 02:31:58 +0530

    Comment out this dangerous script

--- scripts/remig.sh
+++ scripts/remig.sh
@@ -12,24 +12,24 @@
 # delete all existing migrations
 
 
-cd src
+# cd src
 
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+# find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 
-rm db.sqlite3
+# rm db.sqlite3
 
-# recreate initial migrations
-printf "\n\n \033[1;31m Warning!!! update the script to run migrations for all apps\033[0m\n \n\n"
-sleep 2
-python manage.py makemigrations users
-python manage.py makemigrations home
-python manage.py makemigrations courses
-python manage.py makemigrations donations
-python manage.py makemigrations activities
+# # recreate initial migrations
+# printf "\n\n \033[1;31m Warning!!! update the script to run migrations for all apps\033[0m\n \n\n"
+# sleep 2
+# python manage.py makemigrations users
+# python manage.py makemigrations home
+# python manage.py makemigrations courses
+# python manage.py makemigrations donations
+# python manage.py makemigrations activities
 
 
-python manage.py migrate
+# python manage.py migrate
 
-# create superuser
-printf "\n\n \033[1;31m CREATE SUPER USER\033[0m\n \n\n"
-python manage.py createsuperuser
+# # create superuser
+# printf "\n\n \033[1;31m CREATE SUPER USER\033[0m\n \n\n"
+# python manage.py createsuperuser
